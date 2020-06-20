cc1272bd0bc667fa722a1f80d8733635ac66fab7
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-20 20:35:17 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = {"name" = "Faith Gicheha" , "height"= "very tall" , "Job"="student"}
+my_hash = {"name" => "Faith Gicheha" , "height"=> "very tall" , "Job"=>"student"}
 end
 
 
