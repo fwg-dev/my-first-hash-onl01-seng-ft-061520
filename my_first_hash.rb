089f34effaba30222ff175c73e2af09b9afb08bf
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-20 20:36:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,7 +3,6 @@ def my_hash
 my_hash = {"name" => "Faith Gicheha" , "height"=> "very tall" , "Job"=>"student"}
 end
 
-
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
